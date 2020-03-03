6711503b68e162c2b3abee9963fa9a5c6c31cc94
Phoebe T <49921653+adironene@users.noreply.github.com>
2020-03-02 22:38:48 -0800

    back up just in case we screw something up!

--- backUpWaterenhance.cpp
+++ backUpWaterenhance.cpp
@@ -4,88 +4,72 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <math.h>
 #include <stdio.h>
-
 using namespace std;
 using namespace cv;
 
+void balance_white(cv::Mat mat) { 
+  double discard_ratio = 0.05;
+  int hists[3][256];
+  memset(hists, 0, 3*256*sizeof(int));
+
+  for (int y = 0; y < mat.rows; ++y) {
+    uchar* ptr = mat.ptr<uchar>(y);
+    for (int x = 0; x < mat.cols; ++x) {
+      for (int j = 0; j < 3; ++j) {
+        hists[j][ptr[x * 3 + j]] += 1;
+      }
+    }
+  }
+
+  // cumulative hist
+  int total = mat.cols*mat.rows;
+  int vmin[3], vmax[3];
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 255; ++j) {
+      hists[i][j + 1] += hists[i][j];
+    }
+    vmin[i] = 0;
+    vmax[i] = 255;
+    while (hists[i][vmin[i]] < discard_ratio * total)
+      vmin[i] += 1;
+    while (hists[i][vmax[i]] > (1 - discard_ratio) * total)
+      vmax[i] -= 1;
+    if (vmax[i] < 255 - 1)
+      vmax[i] += 1;
+  }
+
+
+  for (int y = 0; y < mat.rows; ++y) {
+    uchar* ptr = mat.ptr<uchar>(y);
+    for (int x = 0; x < mat.cols; ++x) {
+      for (int j = 0; j < 3; ++j) {
+        int val = ptr[x * 3 + j];
+        if (val < vmin[j])
+          val = vmin[j];
+        if (val > vmax[j])
+          val = vmax[j];
+        ptr[x * 3 + j] = static_cast<uchar>((val - vmin[j]) * 255.0 / (vmax[j] - vmin[j]));
+      }
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
-
     Mat image = imread(argv[1], 1);
-    int height = image.rows;
-    int width = image.cols;
-    // if (!image.data)
-    // {
-    //     printf("No image data \n");
-    //     return -1;
-    // }
+    int beforeHeight = image.rows; 
+    int beforeWidth = image.cols; 
+    int k; // whatever scale
+    @TODO assign k
+
     namedWindow("before", WINDOW_AUTOSIZE);
+    
+    resize(image, image, Size(beforeWidth*k, beforeHeight*k)); 
     imshow("before", image);
-    //img enhance
-    /*
-    ya
-    yeet
-    yeet
-    yeet
-    */
-    Mat results[3];
+    balance_white(image); 
+    namedWindow("with white balance", WINDOW_AUTOSIZE);
+    imshow("with white balance", image);
 
-    int ro = image.rows;
-    int co = image.cols;
-    int chan = image.channels();
-    double thirdPercent = 0.16667;
-    vector<Mat> rgbChannels(3);
-    if (chan == 3)
-        cv::split(image, rgbChannels);
-    else
-        rgbChannels.push_back(image);
-
-    for (int i = 0; i < chan; i++)
-    {
-        Mat flat;
-        rgbChannels[i].reshape(1, 1).copyTo(flat);
-        cv::sort(flat, flat, SORT_ASCENDING);
-        std::cout << (int)floor(flat.cols * thirdPercent)<< std::endl;
-        std::cout << (int)ceil(flat.cols * (1.0 - thirdPercent))<< std::endl;
-        double lowVal = flat.at<Vec3b>(0, (int)floor(flat.cols * thirdPercent))[0];
-        std::cout << lowVal<< std::endl;
-        double topVal = flat.at<Vec3b>(0, (int)ceil(flat.cols * (1.0 - thirdPercent)))[0];
-        std::cout << topVal<< std::endl;
-        Mat channel = rgbChannels[i];
-        for (int m = 0; m < ro; m++)
-        {
-            for (int n = 0; n < co; n++)
-            {
-                
-                 if (channel.at<Vec3b>(m, n)[0] < lowVal)
-                      channel.at<cv::Vec3b>(m, n)[0]= lowVal;
-                
-                 if (channel.at<Vec3b>(m, n)[0] > topVal)
-                     channel.at<cv::Vec3b>(m, n)[0] = topVal;
-            }
-        }
-            cv::normalize(channel, channel, 0.0, 255.0 / 2, NORM_MINMAX, -1);
-
-            stringstream ss;
-            ss << i;
-            string str = ss.str();
-            // std::cout << str<< std::endl;
-
-            cv::namedWindow(str, WINDOW_AUTOSIZE);
-                imshow(str, channel);
-            results[i]= channel;
-        //imshow("sep" + i, results[i]);
-    }
-
-    Mat outval = Mat();
-    // Mat outval;
-    cv::merge(results, 3, outval);
-    Mat test = outval;
-    // auto end = chrono::high_resolution_clock::now();
-    // auto dur = end - begin;
-    // auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
-    // cout << ms << endl;
-    imshow("after", outval);
     waitKey(0);
     return 0;
 }
